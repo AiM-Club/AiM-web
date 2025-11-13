b733104419b031ca863e4026ce5aa03c6dc96b10
Junmingeun <junminguen@naver.com>
2025-11-13 18:12:00 +0900

    feat: 메인페이지 연결 및 defaultlayout 적용

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import DefaultLayout from "@/layouts/defaultLayout/DefaultLayout";
+
+const Home = () => {
+    
+    return(
+        <DefaultLayout>
+            <div>
+                hello
+            </div>
+        </DefaultLayout>
+    )
+}
+
+export default Home;
