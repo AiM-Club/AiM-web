7eb9181fff2743833985e2bfd5d894866628972b
soyeon <soyeon20031010@gmail.com>
2025-11-26 01:25:44 +0900

    fix: 파일 오타 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { PageEndPoints } from "./constants/endpoints"
-import { Login } from "./pages/login/Lgoin"
+import { Login } from "./pages/login/Login"
 import Home from "./pages/home/Home"
 
 
