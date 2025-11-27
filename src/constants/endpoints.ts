3d1afdf22af971ec289cf0f9334fd0dff5ea8134
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-11-27 18:06:18 +0900

    fix: 오류 수정

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -2,6 +2,6 @@ export enum PageEndPoints {
     HOME = "/",
     MYPAGE="/mypage",
     COMMUNITY="/community",
-    CHALLENGE_MAIN="/challenge"
+    CHALLENGE_MAIN="/challenge",
     LOGIN = "/login",
 }
