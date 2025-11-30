e412bc1ca3a90cecc8d06a4119af9c9947903bca
soyeon <soyeon20031010@gmail.com>
2025-11-30 23:27:49 +0900

    feat: error component 생성

---
+++ src/components/error/FieldError.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FieldErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font: var(--body-r-l);
+  color: var(--state-error);
+  gap: 0.375rem;
+  margin-top: 0.375rem;
+`;
