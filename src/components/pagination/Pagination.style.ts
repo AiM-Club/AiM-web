5ddd5ebddf3ab754dec1a144e1c000c54f05e96a
Junmingeun <junminguen@naver.com>
2025-12-04 17:04:43 +0900

    feat: pagination component 생성

---
+++ src/components/pagination/Pagination.style.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import Button from "../button/Button";
+
+export const PaginationWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 2.875rem;
+    align-items: center;
+    gap: 5.75rem;
+`;
+
+export const PaginationItem = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const PageInfo = styled.div`
+    display: flex;
+    justify-content: center;
+    font: var(--body-r-l);
+    background-color: var(--surpace-primary);
+    height: 2.875rem;
+    text-align: center;
+    padding: 0 2rem;
+    align-items: center;
+    border-radius: 0.25rem;
+`;
+
+export const PaginationButton = styled.button<{ $isClicked: boolean }>`
+    height: 2.875rem;
+    color: var(--text-primary-default);
+`;
---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,26 @@
+import * as S from "./Pagination.style";
+import { MdOutlineNavigateBefore, MdOutlineNavigateNext } from "react-icons/md";
+
+interface PaginationProps {
+    currentPage: number;
+    totalPage: number;
+    callback: (page: number) => void;
+}
+
+const Pagination = ({ currentPage, totalPage, callback }: PaginationProps) => {
+    return (
+        <S.PaginationWrapper>
+            <S.PaginationButton $isClicked={currentPage === 1} onClick={() => callback(currentPage - 1)}>
+                <MdOutlineNavigateBefore size={48} />
+            </S.PaginationButton>
+            <S.PageInfo>
+                {currentPage}/{totalPage}
+            </S.PageInfo>
+            <S.PaginationButton $isClicked={currentPage === totalPage} onClick={() => callback(currentPage - 1)}>
+                <MdOutlineNavigateNext size={48} />
+            </S.PaginationButton>
+        </S.PaginationWrapper>
+    )
+}
+
+export default Pagination;
