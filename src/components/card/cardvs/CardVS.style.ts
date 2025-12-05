80a640896210be8893d6ad3c324b3c1c0264df27
Junmingeun <junminguen@naver.com>
2025-12-05 17:09:08 +0900

    feat: cardvs 수정

--- src/components/card/cardvs/CardVS.style.ts
+++ src/components/card/cardvs/CardVS.style.ts
@@ -29,34 +29,26 @@ export const RankImg = styled.img`
 `;
 
 export const CardWrapper = styled.div`
-  flex: 1;
-  min-width: 16.625rem;
-  height: 26.313rem;
-  margin: 0.375rem;
+  min-width: 17.375rem;
+  height: 27.063rem;
+  box-sizing: border-box;
+  padding: 0.375rem;
   display: flex;
   flex-direction: column;
 
   &:hover {
     min-width: 17.375rem;
     height: 27.063rem;
-    margin: 0;
     background-image: url(${CardVSHover});
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-    background-position: center;
   }
 
   &:hover ${CardContent} {
-    margin: 0.375rem;
     border: 3px solid var(--border-primary-hover);
+    background-color: var(--surpace-primary);
   }
 
   &:hover ${UserName} {
     background-image: url(${VSTitleHover});
-  }
-
-  @media (max-width: 1378px) {
-    flex: 0 1 calc(50% - 0.5rem);
   }
 `;
 
