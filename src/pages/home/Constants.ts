c859b67c459d9c616f869ff60159e23c16292444
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-12-30 21:32:11 +0900

    feat: 홈페이지 vs 분야 추가

--- src/pages/home/Constants.ts
+++ src/pages/home/Constants.ts
@@ -1,5 +1,16 @@
 import Profile from "@/assets/ProfileImg.png";
 import type { CardVSProps } from "@/types/VSBattle";
+import AllField from "@/assets/field/AllField.svg";
+import IT from "@/assets/field/ITField.svg";
+import Economy from "@/assets/field/EconomyField.svg";
+import Manage from "@/assets/field/ManageField.svg";
+import Politics from "@/assets/field/PoliticsField.svg";
+import Language from "@/assets/field/LanguageField.svg";
+import Nature from "@/assets/field/NatureField.svg";
+import Design from "@/assets/field/DesignField.svg";
+import Music from "@/assets/field/MusicField.svg";
+import PE from "@/assets/field/PEField.svg";
+
 
 export const cardVSData: CardVSProps[] = [
     {id:1, userImg:Profile, userName:"작성자", startTime: "2025.11.07", term: "n주", 
@@ -13,5 +24,22 @@ export const cardVSData: CardVSProps[] = [
     {id:3, userImg:Profile, userName:"작성자", startTime: "2025.11.07", term: "n주", 
         rank:"bronze", img: Profile, title: "제목 작성은 15글자 이하", 
         field: ["분야","분야","분야"], tag: ["태그","태그","태그"], job: "직무", like:22
+    },
+    {id:4, userImg:Profile, userName:"작성자", startTime: "2025.11.07", term: "n주", 
+        rank:"bronze", img: Profile, title: "제목 작성은 15글자 이하", 
+        field: ["분야","분야","분야"], tag: ["태그","태그","태그"], job: "직무", like:22
     }
+];
+
+export const fieldData = [
+    {id:1, name:"분야1", img: AllField},
+    {id:2, name:"분야2", img: IT},
+    {id:3, name:"분야3", img: Economy},
+    {id:4, name:"분야4", img: Manage},
+    {id:5, name:"분야5", img: Politics},
+    {id:6, name:"분야6", img: Language},
+    {id:7, name:"분야7", img: Nature},
+    {id:8, name:"분야8", img: Design},
+    {id:9, name:"분야9", img: Music},
+    {id:10, name:"분야10", img: PE}
 ];
--- src/styles/home/Home.style.ts
+++ src/styles/home/Home.style.ts
@@ -4,12 +4,12 @@ export const HomeWrapper = styled.div`
   width: calc(85%);
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 5rem;
   margin: 2rem auto;
 `;
 
 export const CardWrapper = styled.div`
-  margin-top: 2rem;
+  margin: 2rem 0;
   width: 100%;
   display: flex;
   gap: 1rem;
@@ -26,9 +26,59 @@ export const CardWrapper = styled.div`
   }
 `;
 
+export const FieldWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+export const FieldList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: flex-start;
+`;
+
+export const FieldItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  flex: 0 0 calc((100% - 9rem) / 10);
+  min-width: 5.688rem;
+  font: var(--test-primary-default);
+  cursor: pointer;
+  
+  @media (max-width: 1200px) {
+    flex: 0 0 calc((100% - 8rem) / 9);
+  }
+  
+  @media (max-width: 1000px) {
+    flex: 0 0 calc((100% - 7rem) / 8);
+  }
+  
+  @media (max-width: 800px) {
+    flex: 0 0 calc((100% - 6rem) / 7);
+  }
+  
+  @media (max-width: 600px) {
+    flex: 0 0 calc((100% - 5rem) / 6);
+  }
+`;
+
 export const BattleWrapper = styled.div`
   margin-top: 2rem;
   width: 100%;
   display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+`;
+
+export const BattleTitle = styled.div`
+  display: flex;
+  align-items: center; 
+  justify-content: space-between;
   gap: 1rem;
 `;
