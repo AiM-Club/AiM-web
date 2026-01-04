6a7c20ccf5a2612e23e998965c05176a497590bd
soyeon <soyeon20031010@gmail.com>
2026-01-04 15:14:12 +0900

    fix: 챌린지 메인페이지 카드 padding 수정

--- src/pages/challenge/Challenge.tsx
+++ src/pages/challenge/Challenge.tsx
@@ -1,7 +1,7 @@
 import { ProgressBar } from "@/components/bar/ProgressBar";
 import { PageTopic } from "@/components/text/PageTopic";
 import DefaultLayout from "@/layouts/defaultLayout/DefaultLayout";
-import * as S from "@/styles/Challenge.style";
+import * as S from "@/styles/challenge/Challenge.style";
 import { cardVSData } from "./Constants";
 import { CardChallenge } from "@/components/card/cardChallenge/CardChallenge";
 import { ChallengeMainContent } from "@/components/content/CardContent";
