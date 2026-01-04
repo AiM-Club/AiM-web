3e8586e8daf10a252d843aafaba8d2f9ae2cd597
soyeon <soyeon20031010@gmail.com>
2026-01-04 23:13:47 +0900

    feat: 챌린지 솔로페이지 구현

---
+++ src/styles/challenge/challengeSolo/ChallengeVSSoloDetail.style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ChallengeVSSoloDetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+`;
+
+export const ChallengeVSSoloDetailContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  padding: 0 7.625rem;
+`;
