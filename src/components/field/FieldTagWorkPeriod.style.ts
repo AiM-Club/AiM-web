49313adfb1bce23f8f741b013c62fa1f795cde27
soyeon <soyeon20031010@gmail.com>
2026-01-04 15:05:17 +0900

    feat: 대결 상세 윗부분 분야, 태그, 직무, 기간 컴포넌트 구현

---
+++ src/components/field/FieldTagWorkPeriod.style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const FieldWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+export const TopWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 1.5rem 3rem;
+`;
+export const BottomWrapper = styled.div``;
+
+export const EachWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Topic = styled.p`
+  flex-shrink: 0;
+  font: var(--subtitle-m-l);
+  color: var(--text-secondary);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  flex-shrink: 0;
+  align-items: center;
+  color: var(--text-primary-default);
+  font: var(--subtitle-m-l);
+  gap: 0.75rem;
+`;
