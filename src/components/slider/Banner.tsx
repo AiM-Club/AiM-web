9ddd6e3b55b3190f156622f002971a00722ce615
soyeon <soyeon20031010@gmail.com>
2026-01-15 16:46:18 +0900

    fix: 작성자 이름, 하트 추가

--- src/components/slider/Banner.tsx
+++ src/components/slider/Banner.tsx
@@ -1,11 +1,34 @@
 import * as S from "./Banner.style";
+import Heart from "@/assets/Heart.svg";
+import HeartFill from "@/assets/ClickedHeart.svg";
+import { useState } from "react";
 
-const Banner = ({ image, topic }: { image: string, topic: string }) => {
+interface BannerProps {
+  image: string;
+  topic: string;
+  writer?: string;
+}
+
+const Banner = ({ image, topic, writer }: BannerProps) => {
+  const [isHeartClicked, setIsHeartClicked] = useState<boolean>(false);
+  const logined = true;
+
   return (
     <S.BannerWrapper>
       <S.BannerImage src={image} />
       <S.BannerOverlay />
-      <S.BannerContent>{topic}</S.BannerContent>
+      <S.BannerContentWrapper>
+        <S.BannerContent>{topic}</S.BannerContent>
+        {writer &&
+          <S.WriterWrapper>
+            <p>{writer}</p>
+            {logined && <S.HeartWrapper>
+              <img src={isHeartClicked ? HeartFill : Heart} onClick={() => setIsHeartClicked(!isHeartClicked)} />
+              <p>10</p>
+            </S.HeartWrapper>}
+          </S.WriterWrapper>
+        }
+      </S.BannerContentWrapper>
     </S.BannerWrapper>
   )
 }
