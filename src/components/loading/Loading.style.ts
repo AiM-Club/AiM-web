e964f736ca7ccc2bee9891b64805eb814eea0e27
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-01-18 23:27:34 +0900

    feat: 로딩페이지 ui 구현

---
+++ src/components/loading/Loading.style.ts
@@ -0,0 +1,41 @@
+import styled, { keyframes } from "styled-components";
+
+const rotate = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+export const LoadingContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+
+  background: linear-gradient(
+    to bottom,
+    var(--background-secondary),
+    var(--background-primary)
+  );
+`;
+
+export const LoadingSpinner = styled.div`
+  --d: 2rem;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  color: var(--pink-800);
+  box-shadow:
+    calc(1 * var(--d)) calc(0 * var(--d)) 0 0,
+    calc(0.707 * var(--d)) calc(0.707 * var(--d)) 0 1px,
+    calc(0 * var(--d)) calc(1 * var(--d)) 0 2px,
+    calc(-0.707 * var(--d)) calc(0.707 * var(--d)) 0 3px,
+    calc(-1 * var(--d)) calc(0 * var(--d)) 0 4px,
+    calc(-0.707 * var(--d)) calc(-0.707 * var(--d)) 0 5px,
+    calc(0 * var(--d)) calc(-1 * var(--d)) 0 6px;
+  animation: ${rotate} 1s infinite steps(8);
+`;
