0ed2d03c55448cda88d329f0c93fb1db4851d037
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-01-19 00:39:06 +0900

    feat: 로그인 접근권한 설정

--- src/pages/challenge/challengeSolo/ChallengeVSSolo.tsx
+++ src/pages/challenge/challengeSolo/ChallengeVSSolo.tsx
@@ -7,22 +7,33 @@ import SearchField from "@/components/field/SearchField";
 import Button from "@/components/button/Button";
 import { useNavigate } from "react-router-dom";
 import { PageEndPoints } from "@/constants/endpoints";
+import { useAuthStore } from "@/stores/authStore";
+import Lock from "@/assets/Lock.svg";
 
 const ChallengeVSSolo = () => {
   const navigate = useNavigate();
+  const { user } = useAuthStore();
+
   return (
     <DefaultLayout>
       <S.ChallengeVSMainWrapper>
         <PageTopic text="SOLO 챌린지" size="l" />
-        <S.ContentWrapper>
-          <SearchField
-            categories={[
-              { value: "ongoing", label: "진행 중" },
-              { value: "completed", label: "진행 완료" }
-            ]}
-          />
-          <CardBoard data={searchVsData} />
-        </S.ContentWrapper>
+        {user ? (
+          <S.ContentWrapper>
+            <SearchField
+              categories={[
+                { value: "ongoing", label: "진행 중" },
+                { value: "completed", label: "진행 완료" }
+              ]}
+            />
+            <CardBoard data={searchVsData} />
+          </S.ContentWrapper>
+        ) : (
+          <S.EmptyState>
+            <S.LockImage src={Lock} />
+            로그인 후 이용 가능합니다
+          </S.EmptyState>
+        )}
       </S.ChallengeVSMainWrapper>
       <Button $variant="fixed" $color="green" onClick={() => navigate(PageEndPoints.CHALLENGE_CREATE)}>작성</Button>
     </DefaultLayout>
