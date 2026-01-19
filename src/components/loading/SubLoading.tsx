ba394da78acef38350a56d9993efc97ca64df886
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-01-20 01:08:11 +0900

    feat: SubLoading 창 ui 구현

---
+++ src/components/loading/SubLoading.tsx
@@ -0,0 +1,11 @@
+import * as S from "./SubLoading.style";
+
+const SubLoading = () => {
+    return (
+        <S.SubLoadingContainer>
+            <S.SubLoadingSpinner />
+        </S.SubLoadingContainer>
+    );
+};
+
+export default SubLoading;
