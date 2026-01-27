00724d6df8bc06c24256b69f0d2cf08ef3493259
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-01-27 15:49:19 +0900

    chore: response 수정

--- src/types/challengeDetail.ts
+++ src/types/challengeDetail.ts
@@ -66,6 +66,7 @@ export interface ProgressList {
     proofImages: ImageResponse[];
     proofFiles: ImageResponse[];
     stopwatchTimeSeconds: number;
+    weeklyStatus: string;
     isComplete: boolean;
 }
 
