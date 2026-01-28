974b1271a59d1bf5bb9d3839862cdd4922362462
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-01-29 00:49:53 +0900

    fix: 챌린지 vs목록 롤백

--- src/pages/challenge/challengeVS/ChallengeVS.tsx
+++ src/pages/challenge/challengeVS/ChallengeVS.tsx
@@ -4,7 +4,7 @@ import CardBoard from "@/components/board/CardBoard";
 import Button from "@/components/button/Button";
 import { PageTopic } from "@/components/text/PageTopic";
 import SearchField from "@/components/field/SearchField";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { PageEndPoints } from "@/constants/endpoints";
 import { useAuthStore } from "@/stores/authStore";
 import Lock from "@/assets/Lock.svg";
@@ -12,39 +12,20 @@ import { useEffect } from "react";
 import usePagination from "@/hooks/usePagination";
 import { useGetChallengeVS } from "@/api/challenge";
 import useSearch from "@/hooks/useSearch";
-import ChallengeVSInvite from "./ChallengeVSInvite";
 
 const ChallengeVS = () => {
-  const location = useLocation();
   const navigate = useNavigate();
   const { user } = useAuthStore();
   const { currentPage, totalPage, setTotalPage, handlePageChange } = usePagination();
-  const categoryFromUrl = new URLSearchParams(location.search).get("category") === "invite" ? "INVITATION" : "ALL";
   const { category, keyword, sort, handleCategoryChange, handleKeywordChange, handleSortChange } = useSearch({
-    initialCategory: categoryFromUrl,
     onSearchChange: () => handlePageChange(1),
   });
   const { data: challengeVSList, isLoading } = useGetChallengeVS({ filterType: category, sort: sort, page: currentPage - 1, size: 16, keyword });
 
   useEffect(() => {
-    if (category === "INVITATION") {
-      const params = new URLSearchParams(location.search);
-      params.set("category", "invite");
-      navigate({ pathname: PageEndPoints.CHALLENGE_VS, search: params.toString() });
-    } else if (category === "ALL") {
-      const params = new URLSearchParams(location.search);
-      params.delete("category");
-      navigate({ pathname: PageEndPoints.CHALLENGE_VS, search: params.toString() });
-    } else if (category === "MY") {
-      const params = new URLSearchParams(location.search);
-      params.set("category", "my");
-      navigate({ pathname: PageEndPoints.CHALLENGE_VS, search: params.toString() });
-    }
-  }, [category]);
-
-  useEffect(() => {
     setTotalPage(challengeVSList?.data.page.totalPages || 1);
   }, [challengeVSList?.data.page.totalPages, setTotalPage]);
+
 
   return (
     <DefaultLayout>
@@ -75,11 +56,7 @@ const ChallengeVS = () => {
               로그인 후 이용 가능합니다
             </S.EmptyState>
           ) : (
-            category == "INVITATION" ? (
-              <ChallengeVSInvite />
-            ) : (
-              <CardBoard data={challengeVSList?.data.content || []} currentPage={currentPage} totalPage={totalPage} handlePageChange={handlePageChange} isLoading={isLoading} type="vs" />
-            )
+            <CardBoard data={challengeVSList?.data.content || []} currentPage={currentPage} totalPage={totalPage} handlePageChange={handlePageChange} isLoading={isLoading} type="vs" />
           )}
         </S.ContentWrapper>
       </S.ChallengeVSWrapper>
