f3dc10c8b40e194019831526abfb7a2960df3a75
Junmingeun <junminguen@naver.com>
2026-02-02 15:41:31 +0900

    feat: 마이페이지 내 페이지 비로그인 접근 제한

--- src/pages/mypage/myPost/MyPost.tsx
+++ src/pages/mypage/myPost/MyPost.tsx
@@ -4,10 +4,15 @@ import { searchVsData } from "@/pages/search/Constants";
 import CardBoard from "@/components/board/CardBoard";
 import SearchField from "@/components/field/SearchField";
 import * as S from "@/styles/mypage/myLiked/myLiked.style";
+import { useAuthStore } from "@/stores/authStore";
+import Lock from "@/assets/Lock.svg";
 
 const MyPost = () => {
+    const {user} = useAuthStore();
+
     return (
         <DefaultLayout>
+            {user ? (
             <S.myLikedWrapper>
                 <PageTopic text="내 게시글" size="l" />
                 <S.ContentWrapper>
@@ -21,6 +26,12 @@ const MyPost = () => {
                     <CardBoard data={searchVsData}/>
                 </S.ContentWrapper>
             </S.myLikedWrapper>
+            ):(
+                <S.EmptyState>
+                    <S.LockImage src={Lock} />
+                    로그인 후 이용 가능합니다
+                </S.EmptyState>
+            )}
         </DefaultLayout>
     )
 }
