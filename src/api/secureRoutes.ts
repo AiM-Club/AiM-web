dd329bcc895468f206d966a7096625b3ebca00ad
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-02-03 18:02:47 +0900

    chore: 리뷰디테일 로그인 관련 적용

--- src/api/secureRoutes.ts
+++ src/api/secureRoutes.ts
@@ -32,4 +32,5 @@ export const secureRoutes = [
     { method: Method.POST, url: ApiEndpoints.QNA },
     { method: Method.GET, url: ApiEndpoints.QNA_DETAIL },
     { method: Method.GET, url: ApiEndpoints.USER_CHALLENGE_RECORD },
+    { method: Method.GET, url: ApiEndpoints.REVIEW_DETAIL },
 ];
