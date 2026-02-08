5a27f4842d955c0973818a791012798ceabb9ada
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2026-02-08 17:27:33 +0900

    chore: vs초대 비로그인 접근 제한 설정

--- src/pages/challenge/challengeVS/ChallengeVSInvite.tsx
+++ src/pages/challenge/challengeVS/ChallengeVSInvite.tsx
@@ -9,13 +9,26 @@ import usePagination from "@/hooks/usePagination";
 import useSearch from "@/hooks/useSearch";
 import { useEffect } from "react";
 import SubLoading from "@/components/loading/SubLoading";
+import { useAuthStore } from "@/stores/authStore";
+import Lock from "@/assets/Lock.svg";
+import { useNavigate } from "react-router-dom";
+import { PageEndPoints } from "@/constants/endpoints";
 
 const ChallengeVSInvite = () => {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
   const { currentPage, totalPage, setTotalPage, handlePageChange } = usePagination();
   const { keyword, sort, handleKeywordChange, handleSortChange } = useSearch({
     onSearchChange: () => handlePageChange(1),
   });
   const { data: challengeRequestData, isLoading } = useGetChallengeRequest({ sort: sort, page: currentPage - 1, size: 16, keyword });
+
+  // ALL이나 MY 클릭 시 ChallengeVS로 이동
+  const handleCategoryChange = (category: string) => {
+    if (category === "ALL" || category === "MY") {
+      navigate(`${PageEndPoints.CHALLENGE_VS}`);
+    }
+  };
 
   useEffect(() => {
     setTotalPage(challengeRequestData?.data.page.totalPages || 1);
@@ -27,25 +40,39 @@ const ChallengeVSInvite = () => {
         <PageTopic text="VS 챌린지" size="l" />
         <S.ContentWrapper>
           <SearchField
+            categories={[
+              { value: "ALL", label: "ALL" },
+              { value: "MY", label: "MY" },
+              { value: "INVITATION", label: "VS 초대" }
+            ]}
+            defaultCategory="INVITATION"
             sorts={[
               { value: "LATEST", label: "최신순" },
               { value: "OLDEST", label: "오래된순" },
               { value: "TITLE", label: "가나다순" },
             ]}
+            onCategoryChange={handleCategoryChange}
             onKeywordChange={handleKeywordChange}
             onSortChange={handleSortChange}
           />
-          <S.InviteContentWrapper>
-            {isLoading && <SubLoading />}
-            {challengeRequestData?.data.content.map((item) => (
-              <InviteContent key={item.id} item={item} />
-            ))}
-            {challengeRequestData?.data.content.length === 0 &&
-              <S.EmptyState>
-                검색 결과가 없습니다
-              </S.EmptyState>
-            }
-          </S.InviteContentWrapper>
+          {user ? (
+            <S.InviteContentWrapper>
+              {isLoading && <SubLoading />}
+              {challengeRequestData?.data.content.map((item) => (
+                <InviteContent key={item.id} item={item} />
+              ))}
+              {challengeRequestData?.data.content.length === 0 &&
+                <S.EmptyState>
+                  검색 결과가 없습니다
+                </S.EmptyState>
+              }
+            </S.InviteContentWrapper>
+          ) : (
+            <S.LockEmptyState>
+              <S.LockImage src={Lock} />
+              로그인 후 이용 가능합니다
+            </S.LockEmptyState>
+          )}
         </S.ContentWrapper>
         {challengeRequestData?.data.content?.length && challengeRequestData?.data.content?.length > 0 && (
           <Pagination
--- src/styles/challenge/challengeVS/ChallengeVSInvite.style.ts
+++ src/styles/challenge/challengeVS/ChallengeVSInvite.style.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import SearchFrame from "@/assets/SearchFrame.svg";
+import EmptySpace from "@/assets/EmptySpace.svg";
 
 export const ChallengeVSInviteWrapper = styled.div`
   display: flex;
@@ -42,4 +43,26 @@ export const EmptyState = styled.div`
     background-repeat: no-repeat;
     aspect-ratio: 1196 / 312;
     position: relative;
+`;
+
+export const LockEmptyState = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.75rem;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 30vh;
+    margin: 0 auto;
+    flex-shrink: 0;
+    background-image: url(${EmptySpace});
+    background-size: 100% 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+`;
+
+
+export const LockImage = styled.img`
+    width: 2.5rem;
+    height: 2.5rem;
 `;
